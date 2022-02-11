1738529ac4f3e77772828c0d3ce093a4b9106b54
Mares1492 <msumarok@tlu.ee>
2022-02-11 15:35:52 +0200

    1 tund lõpp

--- 01_funktsioon/Functsioonid.java
+++
@@ -1,9 +0,0 @@
-public class Functsioonid {
-    public static void main(String[] args) {
-        System.out.println(Taksosõit(h:3.5));
-
-        double sõiduMaksumus = taksoSõit()
-            System.out.println()
-        }
-    }
-}
