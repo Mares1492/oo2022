318800ea2cd36ee721f1b524ca3009a986c164da
Mares1492 <msumarok@tlu.ee>
2022-02-15 04:04:47 +0200

    Tehtud(2497 ei tööta)

---
+++ 01_funktsioon/ArabicToRoman.java
@@ -0,0 +1,87 @@
+
+//Kodutöö | 2497 bug
+
+import java.util.Scanner;
+
+public class ArabicToRoman {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number that you would like to convert to roman numerals(from 1 to 3999): ");
+        int userInput = 9001;
+        while (userInput > 4000 || userInput < 0) {
+            userInput = input.nextInt();
+            if (userInput > 4000 || userInput < 0){
+                System.out.println("You have entered the wrong number, please try again");
+            }
+        }
+        String output = String.format("%s in Roman numerals is: ", userInput);
+        System.out.println(output + getRomanNumeral(userInput)); //vastus
+    }
+
+    public static String getRomanNumeral(int number) {
+        if (number == 9001){
+            return "It's over 9000!!!";
+        }
+        int tempNumber = number; // number, mis muutub
+        char[] romanNumerals = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+        int[] arabicNumbers = {1, 5, 10, 50, 100, 500, 1000};
+        int romanNumeralsInArr = 0; //mitu rooma numbri läks sisse
+        char[] tempArr = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',' ',' ',' ',' ',' '};
+        while(tempNumber > 0) { //igaks juhuks tehtud
+            for (int arrPosition = arabicNumbers.length - 1; arrPosition >= 0; arrPosition--) {
+                int loopCounter = 0;
+                while ((tempNumber - arabicNumbers[arrPosition]) >= 0) { //töötab nii palju, kui on võimalik, aga mitte rohkem, kui 4 korda
+                    System.out.println("------------------------------------");
+                    System.out.printf("Before the loop tempNumber is %s %n", tempNumber);
+                    tempArr[romanNumeralsInArr] = romanNumerals[arrPosition];
+                    romanNumeralsInArr += 1;
+                    loopCounter += 1;
+                    tempNumber -= arabicNumbers[arrPosition];
+                    if (loopCounter > 3) {
+                        System.out.printf("Roman numerals in tempArr(before format): %s %n", romanNumeralsInArr);
+                        System.out.printf("At the moment array position is %s %n", arrPosition);
+                        System.out.printf("Before the formatting tempNumber is %s %n", tempNumber);
+                        System.out.println("At the moment tempArr contains: ");
+                        for (int i = 0; i < tempArr.length; i++) {
+                            System.out.println(tempArr[i]);
+                        }
+                        tempArr[romanNumeralsInArr-1] = ' ';
+                        tempArr[romanNumeralsInArr - 2] = ' ';
+                        if (romanNumeralsInArr < 5 || tempNumber % 9 != 0) { // Rooma numbrite spetsiifika
+                            tempArr[romanNumeralsInArr - 3] = romanNumerals[arrPosition+1];
+                            tempArr[romanNumeralsInArr - 4] = romanNumerals[arrPosition];
+                            romanNumeralsInArr = romanNumeralsInArr - 2;
+                        }else {
+                            tempArr[romanNumeralsInArr - 3] = ' ';
+                            tempArr[romanNumeralsInArr - 4] = romanNumerals[arrPosition + 2];
+                            tempArr[romanNumeralsInArr - 5] = romanNumerals[arrPosition];
+                            romanNumeralsInArr = romanNumeralsInArr - 3;
+                        }
+
+                        loopCounter = 0;
+                        System.out.println("----------Formatting---------");
+                        System.out.println();
+                        System.out.printf("Roman numerals in tempArr(after format): %s %n", romanNumeralsInArr);
+                    }
+                    System.out.println("At the moment tempArr contains: ");
+                    for (int i = 0; i < tempArr.length; i++) {
+                        System.out.println(tempArr[i]);
+                    }
+                    System.out.printf("Roman numerals in tempArr: %s %n", romanNumeralsInArr);
+                    System.out.printf("After the loop tempNumber is %s %n", tempNumber);
+                }
+            }
+            if (tempNumber == 0) {
+                System.out.printf("Roman numerals in the final array: %s %n", romanNumeralsInArr);
+                StringBuilder answer = new StringBuilder();
+                for (char numeral : tempArr) {
+                    answer.append(numeral);
+                }
+                return answer.toString();
+            }
+        }
+        return "Whoops";
+    }
+}
+
+
--- 01_funktsioon/Homework.java
+++ 01_funktsioon/Homework.java
@@ -1,4 +1,10 @@
+/*
+
+See on demo, kus ma proovisin enda algoritmi. Töö on failis 'ArabicToRoman'!
+
+ */
 import java.util.Scanner;
+
 
 public class Homework {
     public static void main(String[] args){
@@ -12,54 +18,67 @@ public class Homework {
         char[] romanNumerals = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
         int[] arabicNumbers = {1, 5, 10, 50, 100, 500, 1000};
         int tempNumber = number; // number, mis muutub
-        int romanNumeralsCounterInList = 0; //mitu rooma numbri läksid sisse
-        char[] tempArr = new char[20];
-        for (int arrPosition = 0; arrPosition < arabicNumbers.length; arrPosition++) {
-            System.out.printf("At the moment array positions are %s %n", arrPosition);
-            if (number == arabicNumbers[arrPosition]) {
-                return romanNumerals[arrPosition] + "";
-            } else if (tempNumber < arabicNumbers[arrPosition]) {
-                int loopCounter = 0;
-                while (tempNumber > 0 || loopCounter <= 3) {
-                    if (tempNumber == 0) {
-                        StringBuilder answer = new StringBuilder();
-                        for (char numeral : tempArr) {
-                            answer.append(numeral);
+        int romanNumeralsInList = 0; //mitu rooma numbri läksid sisse
+        char[] tempArr = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+        int loopCounter = 0;
+        while (tempNumber > 0) {
+            for (int arrPosition = arabicNumbers.length - 1 ; arrPosition >= 0 ; arrPosition--) {
+                System.out.printf("At the moment array position is %s %n", arrPosition);
+                int initPos = arrPosition;
+                if (number == arabicNumbers[arrPosition]) {
+                    return romanNumerals[arrPosition] + "";
+                } else if (tempNumber < arabicNumbers[arrPosition]) {
+                    loopCounter = 0;
+                    while (tempNumber > 0 || loopCounter <= 3) {
+                        if (tempNumber == 0) {
+                            StringBuilder answer = new StringBuilder();
+                            for (char numeral : tempArr) {
+                                answer.append(numeral);
+                            }
+                            System.out.printf("At the moment while is %s loops long %n", loopCounter);
+                            return answer.toString();
                         }
-                        return answer.toString();
-                    }
-                    System.out.printf("Before the loop tempNumber is %s %n", tempNumber);
-                    if (tempNumber > 0) {
-                        while ((tempNumber - arabicNumbers[arrPosition - 1]) < 0) {
-                            arrPosition -= 1;
-                            loopCounter = 0;
-                        }
-                    }
-                    if (arrPosition > 0) {
-                        tempArr[romanNumeralsCounterInList] = romanNumerals[arrPosition - 1];
-                        romanNumeralsCounterInList += 1;
-                        tempNumber -= arabicNumbers[arrPosition - 1];
-                    } else {
-                        tempArr[romanNumeralsCounterInList] = romanNumerals[arrPosition];
-                        romanNumeralsCounterInList += 1;
-                        tempNumber -= arabicNumbers[arrPosition];
-                    }
+                        System.out.println("------------------------------------");
+                        System.out.printf("Before the loop tempNumber is %s %n", tempNumber);
+                        if (tempNumber > 0) {
+                            while ((tempNumber - arabicNumbers[arrPosition - 1]) < 0) {
+                                arrPosition -= 1;
+                            }
 
-                    System.out.printf("After the loop tempNumber is %s %n", tempNumber);
-                    loopCounter += 1;
-                    System.out.printf("At the moment while is %s loops long %n", loopCounter);
-                    System.out.println("At the moment tempArr contains: ");
-                    for (int i = 0; i < tempArr.length; i++) {
-                        System.out.println(tempArr[i]);
-                    }
-                }
-                if (loopCounter >= 3) {
-                    tempNumber = number;
-                    for (int arrPositionForNeg = 0; arrPositionForNeg < arabicNumbers.length; arrPositionForNeg++) {
-                        if (tempNumber < arabicNumbers[arrPositionForNeg]) {
-                            char tempBiggy = romanNumerals[arrPositionForNeg];
-                            return "Siin";
                         }
+                        if (arrPosition > 0) {
+                            tempArr[romanNumeralsInList] = romanNumerals[arrPosition - 1];
+                            romanNumeralsInList += 1;
+                            tempNumber -= arabicNumbers[arrPosition - 1];
+                        } else {
+                            tempArr[romanNumeralsInList] = romanNumerals[arrPosition];
+                            romanNumeralsInList += 1;
+                            tempNumber -= arabicNumbers[arrPosition];
+                        }
+
+                        if (loopCounter > 3) {
+                            System.out.printf("Roman numerals in answer(before format): %s %n", romanNumeralsInList);
+                            System.out.printf("At the moment array position is %s %n", arrPosition);
+                            System.out.printf("At the moment Initial position is %s %n", initPos);
+                            tempArr[romanNumeralsInList-1] = ' ';
+                            tempArr[romanNumeralsInList - 2] = ' ';
+                            tempArr[romanNumeralsInList - 3] = ' ';
+                            tempArr[romanNumeralsInList - 4] = romanNumerals[arrPosition+1];
+                            tempArr[romanNumeralsInList - 5] = romanNumerals[arrPosition-1];
+                            tempNumber += arabicNumbers[arrPosition+1] + arabicNumbers[arrPosition-1];
+                            romanNumeralsInList = romanNumeralsInList - 3;
+                            loopCounter = 0;
+                            System.out.printf("Roman numerals in answer(after format): %s %n", romanNumeralsInList);
+                        }
+                        System.out.println("At the moment tempArr contains: ");
+                        for (int i = 0; i < tempArr.length; i++) {
+                            System.out.println(tempArr[i]);
+                        }
+                        System.out.printf("Roman numerals in answer: %s %n", romanNumeralsInList);
+                        System.out.printf("After the loop tempNumber is %s %n", tempNumber);
+                        loopCounter += 1;
+                        System.out.printf("At the moment while is %s loops long %n", loopCounter);
+
                     }
                 }
             }
@@ -68,5 +87,4 @@ public class Homework {
     }
 
 }
-
 
