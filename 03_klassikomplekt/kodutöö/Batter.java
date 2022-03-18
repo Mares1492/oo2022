0f73f962b01fc485e76e0dc41254cec10acbc1b0
Mares1492 <msumarok@tlu.ee>
2022-03-18 13:59:21 +0200

    Kodutöö 3 taastatud

---
+++ 03_klassikomplekt/kodutöö/Batter.java
@@ -0,0 +1,12 @@
+package kodutöö;
+
+public class Batter {
+    int strength;
+    int technique;
+
+    public Batter(int strength, int technique){
+        this.strength = strength;
+        this.technique = technique;
+    }
+
+}
--- 03_klassikomplekt/kodutöö/Main.java
+++ 03_klassikomplekt/kodutöö/Main.java
@@ -1,3 +1,5 @@
+package kodutöö;
+
 public class Main {
     public static void main(String[] args) {
         Field field = new Field();
