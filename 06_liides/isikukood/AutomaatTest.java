eb84182f25ea52f13b9412e0e0e3b59f1960bea0
Mares1492 <msumarok@tlu.ee>
2022-03-24 15:27:38 +0200

    6 kodutöö tehtud

---
+++ 06_liides/isikukood/AutomaatTest.java
@@ -0,0 +1,18 @@
+package isikukood;
+
+import kujundid.Kolmnurk;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class AutomaatTest {
+
+    @Test
+    public void kontrollin_kas_on_naisterahvas_kuiisikukoodi_alguses_on_4(){
+        IsikukoodEE ee = new IsikukoodEE("39508213717");
+        assertEquals(false, ee.isMale());
+    }
+    @Test
+    public void kontrolli_kas_kujundi_ymbermoot_arvutatakse_korretselt() {
+        Kolmnurk kolmnurk = new Kolmnurk(5.0,6.0,7.0);
+        assertEquals(17, kolmnurk.arvutaYmbermoot(),0.1);
+    }
+}
