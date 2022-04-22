10690187a7c6ac4f81ebdf1c185a4af53f47a1ab
Mares1492 <msumarok@tlu.ee>
2022-04-22 15:50:20 +0300

    tund 22.04.22

--- 09_tunnirakenduse_lahendus/src/main/java/mares/webshop/controller/ProductController.java
+++ 09_tunnirakenduse_lahendus/src/main/java/mares/webshop/controller/ProductController.java
@@ -8,34 +8,46 @@ import mares.webshop.repository.ProductRepository;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
 
-    @GetMapping("products") //localhost:8080/products   GET
+    @GetMapping("products") // localhost:8080/products   GET
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
-    @GetMapping("products/{index}")  //localhost:8080/products/1   GET
+
+    @GetMapping("products/{index}") // localhost:8080/products/1  GET
     public Product getProduct(@PathVariable Long index) {
         return productRepository.findById(index).get();
     }
-    @DeleteMapping("products/{index}") //localhost:8080/products/1  DELETE
+
+    @DeleteMapping("products/{index}") // localhost:8080/products/1 DELETE
     public void deleteProduct(@PathVariable Long index) {
         productRepository.deleteById(index);
     }
-    @DeleteMapping("products") //localhost:8080/products   DELETE
+
+    @DeleteMapping("products")  // localhost:8080/products   DELETE
     public String deleteAllProducts() {
         productRepository.deleteAll();
-        return "Kõik toodet kustutatud";
+        return "Kõik tooted kustutatud";
     }
-    @PostMapping("products") //localhost:8080/products  POST
+
+    @PostMapping("products") // localhost:8080/products  POST
     public void addProduct(@RequestBody Product product) {
         productRepository.save(product);
     }
-    @PutMapping("products/{index}") //localhost:8080/products/1
+
+    @PutMapping("products") // localhost:8080/products  PUT
     public void editProduct(@RequestBody Product product) {
         productRepository.save(product);
     }
+
+    @PostMapping("add-all-products") // localhost:8080/products  POST
+    public void addProduct(@RequestBody List<Product> products) {
+        productRepository.saveAll(products);
+    }
 }
+
